8a33ec5269899d3861a86c7640f798e2dea82a03
Joachim Gassen <gassen@wiwi.hu-berlin.de>
2024-11-13 15:52:03 +0100

    refactoring to integrate the Evans et al code into our workflow, added descriptive table

--- code/honesty_create_result_objects.R
+++ code/honesty_create_result_objects.R
@@ -1,5 +1,7 @@
 # --- Honesty: Tables ---------------------------------------------------------- 
 
+EVANS_COND <- c("Evans et al.", "Contextualized", "Neutral") 
+
 honesty_tab_desc_part_period <- function(
     dta = hrounds,
     vars = c("honesty", "truthful", "all_slack"),
@@ -19,6 +21,17 @@ honesty_tab_desc_part <- function(
   desc_table(dta, vars, var_labels, var_tests)
 }
 
+
+honesty_tab_evans_desc <- function(
+    conditions = c("Evans et al.", "Contextualized"),
+    dta = hevans, 
+    vars = c("mn_honesty"),
+    var_labels = c("Mean % Honesty"),
+    var_tests = c("t")
+) {
+  desc_table(dta, vars, var_labels, var_tests, conditions, flip = FALSE)
+}
+
 honesty_tab_regression_results <- function(dta = hrounds) {
   mod_fe <- feols(
     honesty ~ experiment | round, 
@@ -33,6 +46,54 @@ honesty_tab_regression_results <- function(dta = hrounds) {
   reg_table(list(mod_fe, mod_by_rounds))
 }
 
+
+honesty_tab_evans_regression_results <- function(dta = hevans) {
+  mod_fe <- feols(
+    mn_honesty ~ experiment | true_amount, 
+    cluster = c("true_amount"), 
+    data = dta %>% filter(true_amount != 6000)
+  )
+  mod_true_amount_iacted <- feols(
+    mn_honesty ~ experiment*true_amount, 
+    cluster = c("true_amount"), 
+    data = dta %>% filter(true_amount != 6000)
+  )
+
+  coef_map <- c(
+    "Intercept", EVANS_COND[2], EVANS_COND[3],
+    "True Amount", glue("{EVANS_COND[2]} \u00d7 True Amount"),
+    glue("{EVANS_COND[3]} \u00d7 True Amount")
+  )
+  
+  names(coef_map) <- c(
+    "(Intercept)",
+    glue("experiment{EVANS_COND[2]}"),
+    glue("experiment{EVANS_COND[3]}"),
+    "true_amount",
+    glue("experiment{EVANS_COND[2]}:true_amount"),
+    glue("experiment{EVANS_COND[3]}:true_amount")
+  )
+  
+  modelsummary(
+    output = "gt",
+    list(
+      "True Amount Fixed Effects" = mod_fe,
+      "Interacted by True Amount" = mod_true_amount_iacted
+    ),
+    statistic = "{std.error} ({p.value})",
+    shape = term ~ model + statistic,
+    gof_map = list(
+      list(raw = "adj.r.squared", clean = "Adjusted R²", fmt = 3),
+      list(
+        raw = "nobs", clean = "Number of observations",
+        fmt = function(x) format(x, big.mark = ",")
+      )
+    ),
+    coef_map = coef_map
+  )
+}
+
+
 honesty_tab_reasons <- function(
     dta = hreasons,
     vars = c(
@@ -44,13 +105,14 @@ honesty_tab_reasons <- function(
       "Cares About Own Payoff", "Cares About Other Payoff", "Cares About Honesty"
     ),
     var_tests = c("chisq", "chisq", "t", "t", "t")
-  ) {
+) {
   # not sure whether this works
   if (is.null(dta)) stop(
     "Reasons have not been classified for this experimental run yet."
   )
   desc_table(dta, vars, var_labels, var_tests)
 }
+
 
 # --- Honesty Figures ----------------------------------------------------------
 
@@ -102,4 +164,22 @@ honesty_fig_by_period <- function(dta = hrounds) {
     theme_classic(base_size = 12) + 
     scale_color_manual("", values = color_scale, labels = color_scale_labs) +
     theme(plot.title.position =  "plot", legend.position = "bottom")
+}
+
+honesty_fig_evans <- function(dta = hevans) {
+  color_scale_labs <- EVANS_COND
+  color_scale <- RColorBrewer::brewer.pal(3 ,"Set1")
+  ggplot(
+    dta, aes(x = true_amount, y = mn_slack, color = experiment)) +
+    geom_jitter(size = 1) +
+    labs(
+      x = "True Cost",
+      y = "Claimed Slack",
+      color = "Treatment"
+    ) +
+    theme_classic(base_size = 12) +
+    geom_segment(x = 4000, y = 2000, xend = 6000, yend = 0, color = "#E41A1C", lty = 2) + 
+    coord_cartesian(clip = 'off', xlim = c(4000, 6000), ylim = c(0, 2000)) + 
+    scale_color_manual("", values = color_scale, labels = color_scale_labs) +
+    theme(plot.title.position = "plot", legend.position = "bottom")
 }
